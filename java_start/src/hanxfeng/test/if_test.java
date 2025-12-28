6e9ab78a6a66ff1bce2fc9df94c627f885a9d516
hanxfeng <3556050019@qq.com>
2025-12-28 18:51:11 +0800

    添加了if和switch的笔记；添加了三种switch的使用方式的代码

---
+++ java_start/src/hanxfeng/test/if_test.java
@@ -0,0 +1,16 @@
+package hanxfeng.test;
+
+public class if_test {
+    public static void main(String[] args) {
+      int a = 10;
+      if(a>5) {
+          int b = 1;
+      }
+      else if (a>10) {
+          int b = 2;
+      }
+      else {
+          int b = 3;
+      }
+    };
+}
