7fdae563b305f5adcd2b378f77ee5e920b6a00f3
hanxfeng <3556050019@qq.com>
2026-02-03 20:22:16 +0800

    2026/2/3学习记录

--- javaStudy/src/main/java/org/example/Main.java
+++ javaStudy/src/main/java/org/example/Main.java
@@ -4,14 +4,19 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("开始");
+        method1();           // 第1个调用
+        System.out.println("结束");
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    static void method1() {
+        System.out.println("进入method1");
+        method2();           // 第2个调用
+        System.out.println("退出method1");
+    }
+
+    static void method2() {
+        System.out.println("进入method2");
+        System.out.println("退出method2");  // 最后调用，但最先结束！
     }
 }
